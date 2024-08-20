6e6a41f7dd75de1d4e824eb82752a3e719d07f14
shumancheng <Shumancheng0119@gmail.com>
2024-08-20 11:52:32 +0200

    add styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter } from "react-router-dom";
 
-function App() {
-  const [count, setCount] = useState(0)
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Loader,
+  Navbar,
+  Tech,
+  Works,
+} from "./components/components";
 
+const App = () => {
   return (
-    <>
-    <div>shuman cheng CV</div>
-    </>
-  )
-}
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className="relative z-0">
+          <Contact />
 
-export default App
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+};
+
+export default App;
